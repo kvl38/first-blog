0586a22e223e11f62a90eb2b9dd9ec80f9bd92ca
kvl38 <pat38ru@gmail.com>
2020-10-15 18:49:05 +0300

    Add Postgres

--- events/apps.py
+++ events/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class EnentaConfig(AppConfig):
-    name = 'enenta'
+    name = 'events'
--- meawesomeblog/settings.py
+++ meawesomeblog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'events.apps.EnentaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'meawesomeblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'meblog',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '9090',
     }
 }
 
@@ -118,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+Media_Root = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
