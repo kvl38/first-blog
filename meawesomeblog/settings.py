1d9b2516b3237f1d238ca904e97091497b8b4031
kvl38 <pat38ru@gmail.com>
2020-10-15 22:20:32 +0300

    add post model

--- meawesomeblog/settings.py
+++ meawesomeblog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'events.apps.EnentaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
